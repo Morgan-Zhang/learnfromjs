d8b2df8e307bc7c10f17783baeb322d4da2f0525
morgan <zhangteng_yx@126.com>
2017-04-30 21:07:26 +0800

    .

---
+++ js13/router.js
@@ -0,0 +1,22 @@
+// 服务端 router.js
+
+
+app.get('/loadMore', function(req, res) {
+
+	var curIdx = req.query.index;
+	var len = req.query.length;
+	var data = [];
+
+	for(var i = 0; i < len; i++) {
+		data.push('新闻' + (parseInt(curIdx) + i));
+	}
+
+	res.send(data);
+});
+
+
+/**
+ * 发送 GET 请求， 无参数
+ * GET /hello
+ * 返回响应数据
+ */
